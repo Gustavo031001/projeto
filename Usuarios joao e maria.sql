03d3011e693489400f34b215ea382d414227b808
Gustavo0310 <111024445+Gustavo031001@users.noreply.github.com>
2023-11-06 15:07:36 -0300

    Update Usuarios joao e maria.sql

--- Usuarios joao e maria.sql
+++ Usuarios joao e maria.sql
@@ -4,4 +4,4 @@ senha varchar(50),
 perfil varchar(50)
 )
 insert into usuario (login, senha, perfil) values ('Funcionario', '123', 'cliente');
-insert into usuario (login, senha, perfil) values ('Gestor', '456', 'adm');
+insert into usuario (login, senha, perfil) values ('Gestor', 'adm', 'adm');
