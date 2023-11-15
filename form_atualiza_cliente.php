d976ef52569bef3f71d1d8536a087786340573af
Gustavo0310 <111024445+Gustavo031001@users.noreply.github.com>
2023-11-15 12:19:59 -0300

    Update form_atualiza_cliente.php

--- form_atualiza_cliente.php
+++ form_atualiza_cliente.php
@@ -3,7 +3,7 @@
     //recupera o id_cliente´
     $id_cliente = $_GET("id_cliente");
     //consulta os dados do cliente pelo id
-    $consulta_por_id = "SELECT * FROM cliente WHERE id = $id_cliente";
+    $consulta_por_id = "SELECT * FROM cliente WHERE id = $id_gestor";
     //executa consulta_por_id
     $result = pg_query($consulta_por_id);
     //retorna um array com os dados do cliente
@@ -14,17 +14,17 @@
 <DOCTYPE html>
     <html>
         <head>
-            <title>Cadastro de Cliente</title>
+            <title>Cadastro do Gestor</title>
             <meta charset="UTF-8">
         </head>
         <body>
-            <h1>Atualização do Cliente</h1>
+            <h1>Atualização do Gestor</h1>
             <form action="atualiza_cliente.php" method="post">
-                <input type="hidden" name="id_cliente" value="<?php print($cliente['id'])?>">
-                Nome:<input type="text" name="txtNome" value="<?php print($cliente['nome'])?>"><br/>
-                CPF:<input type="text" name="txtCPF" value="<?php print($cliente['cpf'])?>"><br/>
-                Telefone:<input type="text" name="txtTelefone" value="<?php print($cliente['telefone'])?>"><br/>
-                Endereço:<input type="text" name="txtEndereco" value="<?php print($cliente['endereco'])?>"><br/>
+                <input type="hidden" name="id_cliente" value="<?php print($gestor['id'])?>">
+                Nome:<input type="text" name="txtNome" value="<?php print($gestor['nome'])?>"><br/>
+                CPF:<input type="text" name="txtCPF" value="<?php print($gestor['cpf'])?>"><br/>
+                Telefone:<input type="text" name="txtTelefone" value="<?php print($gestor['telefone'])?>"><br/>
+                Endereço:<input type="text" name="txtEndereco" value="<?php print($gestor['endereco'])?>"><br/>
                 <input type="submit" value="Atualizar">
             </form>
         </body>
