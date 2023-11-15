bd5b95295e0f68a49dd7e146b6c5554b76f72356
Gustavo0310 <111024445+Gustavo031001@users.noreply.github.com>
2023-11-15 12:15:08 -0300

    Update lista_cadastros.php

--- lista_cadastros.php
+++ lista_cadastros.php
@@ -10,7 +10,7 @@
             <meta charset="UTF-8">
 </head>
 <body>
-    <h1>Clientes Cadastrados</h1>
+    <h1>Gestor Cadastrado</h1>
     <table border=1>
         <tr>
             <td>Atualizar</td>
@@ -21,22 +21,22 @@
         </tr>
 <?php
 //converte cada registro em um array e exibe na tabela
-        while($cliente = pg_fetch_assoc($result)){
+        while($gestor = pg_fetch_assoc($result)){
             echo "<tr>";
             //passa como parametro via $_GET o id de cada cliente
             echo "<td><a href= 'form_atualiza_cliente.php?id_cliente[id]'> atualizar</a></td>";
             echo "<td><a href= 'exclui_cliente.php?id_cliente=$cliente[id]'> excluir</a></td>";
             //exibe os dados do cliente
-            echo "<td>" .$cliente['nome']. "</td>";
-            echo "<td>" .$cliente['telefone']. "</td>";
-            echo "<td>" .$cliente['endereco']. "</td>";
+            echo "<td>" .$gestor['nome']. "</td>";
+            echo "<td>" .$gestor['telefone']. "</td>";
+            echo "<td>" .$gestor['endereco']. "</td>";
             echo "</tr>";
         }
         //fecha conexao
         pg_close($conexao);
 ?>
 </table>
-<p>Cadastrar <a href="form_cadastra_cliente.html">novo </a>cliente </p>
+<p>Cadastrar <a href="form_cadastra_cliente.html">novo </a>$gestor </p>
     </body>
     </html>
         
